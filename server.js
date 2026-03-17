7e62784709d3c135b907f38697955585c3c2ef1a
Dev_Abdo_A_Mo <abdokhater2000@gmail.com>
2026-03-17 05:35:37 +0200

    port changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const EXCEL_FILE = path.join(__dirname, 'orders.xlsx');
 
 app.use(cors());
